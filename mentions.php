23556e1db373396c62cab6ce97b34797f27d8f58
Vincent_Caraty <vincent.caraty@outlook.fr>
2020-04-24 14:18:48 +0200

    mise a jour des fichiers

---
+++ mentions.php
@@ -0,0 +1,104 @@
+<?php 
+    // ajout du <head>et <header>
+    include 'header.html' ; 
+?>
+
+    <main class="main-base">
+        <div class="bg-cover">
+            <h1>Mentions légales</h1>
+        </div>
+
+        <section class="row max btw">
+
+        <?php
+            // ASIDE LEFT
+            include 'asideLeft.html' ; 
+        ?>
+
+            
+    <div class="container col ard">
+            <article class="col resto-1">
+                <h2>Informations légales</h2>
+                <p>Merci de lire attentivement les présentes modalités d'utilisation du présent site avant de le parcourir. En vous connectant sur ce site, vous acceptez sans réserve les présentes modalités.</p>
+                <h3>Editeur du site</h3>
+                <p>EI Auto New Life- Mr Fabien Delunel<br>
+                181 rue Jean Bernard<br>
+                30700 Foissac<br>
+                autonewlife.contact@gmail.com<br>
+                Siret :853 445 401 00021 (RCS Nîmes)<br>
+                Tel : 06 33 35 33 61<br>
+                www.autonewlife.fr<br>
+                </p>
+                <h3>Conditions d'utilisation</h3>
+                <p>Le site accessible par les url suivants : www.autonewlife.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales.   En utilisant le site, vous reconnaissez avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Auto New Life.Auto New Life saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service. </p>
+                <h3>Responsable éditorial</h3>
+                <p>Site internet édité par Nadège Lagraa/Comteolia (developpement web, design, graphismes, branding et copywritting).
+                nadegelagraa@gmail.com<br>
+                06 67 050 870<br>
+                8 rue de la Coste -30190 Collorgues<br>
+                www.cometeolia.com<br>
+                Siret : 51287501400023</p>
+                <h3>Limitation de responsabilité</h3>
+                <p>Les informations contenues sur ce site sont aussi précises que possibles et le site est périodiquement remis à jour, mais peut toutefois contenir des inexactitudes, des omissions ou des lacunes. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email en décrivant le problème de la manière la plus précise possible (page posant problème, action déclenchante, type d’ordinateur et de navigateur utilisé, …).
+                    
+Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, Natural net ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement.     
+
+Les photos sont non contractuelles.
+
+Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de Auto New life.</p>
+                <h3>Litiges</h3>
+                <p>Les présentes conditions sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le   siège social de la société Auto New Life La langue de référence, pour le règlement de contentieux éventuels, est le français.</p>
+                <h3>Déclaration à la CNIL</h3>
+                <p>Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, le site a fait l'objet d'une déclaration auprès de la Commission nationale de l'informatique et des libertés (www.cnil.fr). </p>
+                <h3>Droit d'accès</h3>
+                <p>En application de cette loi, les internautes disposent d’un droit d’accès, de rectification, de modification et de suppression concernant les données qui les concernent personnellement. Ce droit peut être exercé par voie postale auprès de Auto New Life, 181 rue Jean Bernard, 30700 Foissac ou par voie électronique à l’adresse email suivante : contact.autonewlife@gmail.com
+Les informations personnelles collectées ne sont en aucun cas confiées à des tiers hormis pour l’éventuelle bonne exécution de la prestation commandée par l’internaute.</p>
+                <h3>Propriété intellectuelle</h3>
+                <p>Tout le contenu du présent site, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société Auto New Life à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
+Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Auto New Life Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.3335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
+
+Auto New Life est identiquement propriétaire des "droits des producteurs de bases de données" visés au Livre III, Titre IV, du Code de la Propriété Intellectuelle   (loi n° 98-536 du 1er juillet 1998) relative aux droits d'auteur et aux bases de données.
+
+Les utilisateurs et visiteurs du site internet peuvent mettre en place un hyperlien en direction de ce site, mais uniquement en direction de la page d’accueil, accessible à l’URL suivante : www.autonewlife.fr, à condition que ce lien s’ouvre dans une nouvelle fenêtre. En particulier un lien vers une sous page (« lien profond ») est interdit, ainsi que l’ouverture du présent site au sein d’un cadre (« framing »), sauf l'autorisation expresse et préalable de Auto New Life.
+
+Pour toute demande d'autorisation ou d'information, veuillez nous contacter par email : contact.autonewlife@gmail.com Des conditions spécifiques sont prévues pour la presse.
+
+Par ailleurs, la mise en forme de ce site a nécessité le recours à des sources externes dont nous avons acquis les droits ou dont les droits d'utilisation sont ouverts : Designed by Nadège Lagraa pour Comteolia. Les illustrations ont été acquises sur Pixabay</p>
+                <h3>Hébergeur</h3>
+                <p></p>
+                <h3>Conditions de services</h3>
+                <p>Ce site est proposé en langages HTML5,CSS3,JS,PHP pour un meilleur confort d'utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Safari, Firefox, Chrome,...</p>
+                <h3>Informations et exclusion</h3>
+                <p>Auto New Life met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de Auto New Life, et signaler toutes modifications du site qu'il jugerait utile. Auto New Life n'est en aucun cas responsable de l'utilisation faite de ces informations,   et de tout préjudice direct ou indirect pouvant en découler.</p>
+                <h3>Cookies</h3>
+                <p>Pour des besoins de statistiques et d'affichage, le présent site utilise des cookies. Il s'agit de petits fichiers textes stockés sur votre disque dur afin d'enregistrer des données techniques sur votre navigation. Certaines parties de ce site ne peuvent être fonctionnelle sans l’acceptation de cookies.</p>
+                <h3>Liens hypertextes</h3>
+                <p>Le site internet de Auto New Lifepeuvent offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet.
+
+Auto New Life ne dispose d'aucun moyen pour contrôler les sites en connexion avec ses sites internet. Nous ne répond pas de la disponibilité de tels sites et sources externes, ni ne la garantit. Elle ne peut être tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent pleinement à l'internaute, qui doit se conformer à leurs conditions d'utilisation.<br>
+
+Les utilisateurs, les abonnés et les visiteurs des sites internet de Auto New Life ne peuvent mettre en place un hyperlien en direction de ce site sans l'autorisation expresse et préalable de Auto New Life.<br>
+
+Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction du site internet de Auto New Life, il lui appartiendra d'adresser un email accessible sur le site afin de formuler sa demande de mise en place d'un hyperlien. Auto New Life se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.</p>
+                <h3>Responsabilité</h3>
+                <p>Aucune autre garantie n'est accordée au client ou visiteur, auquel incombe l'obligation de formuler clairement ses besoins et le devoir de s'informer. Si des informations fournies par Auto New Life apparaissent inexactes, il appartiendra au client ou visiteur de procéder lui-même à toutes vérifications de la cohérence ou de la vraisemblance des résultats obtenus. Auto New Life ne sera en aucune façon responsable vis à vis des tiers de l'utilisation par le client  ou le visiteur des informations ou de leur absence contenues dans ses produits y compris un de son site Internet.</p>
+                <h3>Contactez-nous</h3>
+                <p>Auto New Life est à votre disposition pour tous vos commentaires ou suggestions. Vous pouvez nous écrire en français par courrier électronique à : contact.autonewlife@gmail.com</p>
+            </article>
+            
+        </div>       
+            
+            
+            
+            <?php 
+                // ASIDE RIGHT
+                include 'asideRight.html' ; 
+            ?>
+
+        </section>
+    </main>
+    
+<?php
+    // ajout du <footer>
+    include 'footer.html' ; 
+?>
---
+++ restauration.php
@@ -0,0 +1,97 @@
+<?php 
+    // ajout du <head>et <header>
+    include 'header.html' ; 
+?>
+        
+<main class="main-base">
+    <div class="bg-cover">
+        <h1>Restauration</h1>
+    </div>
+    <section  class="row max btw">
+        
+            
+        <?php 
+            // ASIDE LEFT
+            include 'asideLeft.html' ; 
+        ?>  
+
+        <div class="container col ard">
+            <article class="col resto-1">
+                <h2>Restauration des anciennes</h2>
+                <p><span></span>
+                    L'une des principales raison qui m'a amené à faire ce métier
+                    est la <strong>passion des voitures anciennes</strong>. <br>
+                    <strong>Voitures de collection ou youngtimers</strong>, elles procurent
+                    un plaisir immense de conduite en toute liberté que tous
+                    passionné recherche. C'est un héritage à conserver, que l'on
+                    soit collectionneur ou non. <br>
+                    Auto New Life dispose de tous les outils et savoir-faire pour
+                    restaurer vos <strong>autos anciennes</strong>, des plus vieilles au plus récentes,
+                    quelque soit la marque. <br>
+                    Parmi nos plus vibrantes <strong>rénovations de vehicules</strong>, voici un petit aperçu des chantiers
+                    réalisés ou en cours.</p>
+            </article>
+            <article class="col ">
+                <h2>Restaurations réalisées</h2>
+                <div class="resto-2 btw">
+                    <div>
+                        <h3>205 Rallye 1989</h3>
+                        <img src="images/205_rallye.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Mercedes E200 1995</h3>
+                        <img src="images/mercedes.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Audi TT 1995</h3>
+                        <img src="images/audi_tt.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Jeep CJ5 1965</h3>
+                        <img src="images/jeep-cj5.png" alt="">
+                    </div>
+                </div>
+            </article>
+            
+            <article>
+                <h2>Restaurations en cours</h2>
+                <div class="resto-2 btw">
+                    <div>
+                        <h3>Austin Mini 1969</h3>
+                        <img src="images/mini.jpg" alt="">
+                    </div>
+                    <div>
+                        <h3>Daimler DS420</h3>
+                        <img src="images/daimler_ds240.png" alt="">
+                    </div>
+                    <div>
+                        <h3>Votre véhicule</h3>
+                        <img src="images/cocc.png" alt="">
+                    </div>
+                </div>
+            </article>
+            
+            <div class="txt-justify">
+                <h2>Un projet de rénovation ?</h2>
+                <P>Sans engagement, nous pouvons nous déplacer dans un rayon de 30 km autour de Foissac pour vos projets de restauration sur voiture complète ou partielle en non état de rouler.
+                Dans d'autres cas, nous serions heureux de vous accueillir au garage pour diagnostiquer ensemble l'état de vôtre véhicule.</P>
+                <p><strong>Nous sommes affiliés à plusieurs assurances pour une prise en charge rapide des réparations.</strong></p>
+                
+                <div class="text-bigger-2">
+                    <P><i class="fas fa-angle-double-right"></i><a href="contact.html" class="txt-underline">Contactez nous par votre moyen favori pour toute demande</a></P>
+                </div>
+            </div>
+        </div>
+        
+        <?php 
+            // ASIDE RIGHT
+            include 'asideRight.html' ; 
+        ?>
+
+    </section>
+</main>
+
+<?php 
+    // ajout du <footer>
+    include 'footer.html' ; 
+?>
