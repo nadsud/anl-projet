c1a579142ed2b6bbd71500b574238acf8174ec48
nadsud <55386443+nadsud@users.noreply.github.com>
2020-05-07 14:53:40 +0200

    Delete faq.js

--- faq.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll(".question a");
-
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
-
-items.forEach(item => item.addEventListener('click', toggleQuestion));
