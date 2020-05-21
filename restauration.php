493fde994b362a82fca4bb7681bbffddf90b9644
nadsud <55386443+nadsud@users.noreply.github.com>
2020-05-21 14:19:37 +0200

    mis  à jour

---
+++ restauration.php
@@ -0,0 +1,201 @@
+<?php 
+    // ajout du <head> et <header>
+    $title = 'Restauration auto yougtimers-véhicules anciens et neufs';
+    $description = ' Passionné et spécialiste en restauration de youngtimers et autos anciennes. Découvrez en images nos plus belles rénovations auto toutes marques : 205 Rallye,Jeep CJ... ';
+    include 'header.phtml' ;
+?>
+        
+<main class="main-base">
+    <div class="bg-cover">
+        <h1>Restauration</h1>
+    </div>
+    <section  class="row max btw">
+        
+            
+        <?php 
+            // ASIDE LEFT
+            include 'asideLeft.html' ; 
+        ?>  
+
+        <div class="container col ard">
+            <article class="col resto-1 txt-justify">
+                <h2>Restauration des anciennes</h2>
+                <p><span></span>
+                    L'une des principales raison qui m'a amené à faire ce métier
+                    est la <strong>passion des voitures anciennes</strong>. <br>
+                    <strong>Voitures de collection ou youngtimers</strong>, elles procurent
+                    un plaisir immense de conduite en toute liberté que tous
+                    passionné recherche. C'est un héritage à conserver, que l'on
+                    soit collectionneur ou non. <br>Vous êtes de plus en plus nombreux a utiliser vos ancienns au quotidien. Son entretien est un budget et c'est pourquoi il est necessaire de faire appel à un spécialiste comme Auto New Life qui connait bien ces voitures et pourra trouver des pièces et solutions adaptées.<br><br>Notre carrosserie dispose de tous les outils et savoir-faire pour
+                    restaurer vos <strong>autos anciennes</strong>, des plus vieilles au plus récentes,
+                    quelque soit la marque. <br>
+                    Parmi nos plus vibrantes <strong>rénovations de vehicules</strong>, voici un petit aperçu des chantiers
+                    réalisés ou en cours.</p>
+            </article>
+            
+<article class="col">
+    <h2>Restaurations réalisées</h2>
+                
+     <!--ACCORDION 1-->           
+            
+         <div class="effect-box">
+                   
+                <div class="box">
+                    <h3>205 Rallye 1989</h3>
+                </div>   
+                 
+                
+                <div class="box">
+                    <h3>Mercedes E200 1995</h3>
+                </div>
+                    
+                <div class="box">
+                    <h3>Audi TT 1995</h3>
+                </div>      
+        </div>
+                          
+                
+                
+<div class="accordion-container">
+    <div class="accordion clearfix">
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/restauration-205rallye-img1.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/restauration-205rallye-img2.png" />
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/restauration-mercedes-img1.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/restauration-mercedes-img2.png" />
+                </div>
+            </div>
+        </div> 
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/restauration-auditt-img1.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/restauration-auditt-img2.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  <!--ACCORDION 2-->
+        
+        <div class="effect-box">
+                   
+                <div class="box">
+                    <h3>Jeep CJ5 1965</h3>
+                </div>   
+                 
+                
+                <div class="box">
+                     <h3>Clio 16S 1993</h3>
+                </div>
+                    
+                <div class="box">
+                    <h3>Toyota Hillux</h3>
+                </div>      
+        </div> 
+                           
+                
+<div class="accordion-container">
+    <div class="accordion clearfix">
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/restauration-clio16srouge-img1.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/restauration-clio16srouge-img2.png" />
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/restauration-jeepcj5-img1.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/restauration-jeepcj5-img2.png" />
+                </div>
+            </div>
+        </div> 
+        <div class="accordion-item-container">
+            <div class="accordion-item-content">
+                <div class="accordion-item-image">
+                    <img src="images/reparation-toyota.png" />
+                </div>
+                <div class="accordion-item-info">
+                    <img src="images/reparation-toyota-img2.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>       
+            
+</article>
+            
+            
+        <article>
+                <h2>Restaurations en cours</h2>
+              
+                <div class="resto-2 btw">
+                    <h3>Austin Mini 1969</h3>
+                    
+                    <div class="row">
+                        
+                        <img src="images/restauration-mini.png" alt="image austin mini">
+                        
+                    </div>
+                    
+                   <h3>Clio 16S</h3>
+                    <div class="row">
+                       
+                        <img src="images/restuaration-clio16s.png" alt="image clio 16s">
+                    </div>
+                    
+                    <h3>Votre véhicule</h3>
+                    <div class="row">
+                        
+                        <img src="images/restauration-auto-cocc.png" alt="image coccinelle">
+                    </div>
+                </div>
+                
+        </article>
+          
+            <div class="txt-justify">
+                <h2>Un projet de rénovation ?</h2>
+                <P>Sans engagement, nous pouvons nous déplacer dans un rayon de 30 km autour de Foissac pour vos projets de restauration sur voiture complète ou partielle et/ou non roulante.
+                Dans d'autres cas, nous serions heureux de vous accueillir au garage pour diagnostiquer ensemble l'état de vôtre véhicule.</P>
+                <p><strong>Nous sommes affiliés à plusieurs assurances pour une prise en charge rapide des réparations.</strong></p>
+                
+                <div class="text-bigger-3">
+                    <p><i class="fas fa-angle-double-right"></i><a href="contact.php" class="txt-underline"> Contactez nous pour toute demande ou pour prendre RDV</a></p>
+                </div>
+            </div>
+        </div>
+        
+        <?php 
+            // ASIDE RIGHT
+            include 'asideRight.html' ; 
+        ?>
+
+    </section>
+</main>
+
+<?php 
+    // ajout du <footer>
+    include 'footer.html' ; 
+?>
